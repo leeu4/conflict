41d670f21a69d136d4b9f6ad08929c92cf5fe40c
saad <saadfotp@gmail.com>
2023-11-28 18:54:53 +0300

    s

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ console.log("back-end");
 prompt("ss");
 console.log("ziyad is stupid and fat"); 
 console.log("زياد 2");
+// ssddsasdasdasd
+// adsaopdkas[opd]
 
-
-// hello 
-
+for (let i = 0; i < 5;i++){
+    console.log(i);
+}
